5c3b463035da4d3f7557b7668b9d269d128cb1eb
Maria Ungur <mariau@maria-mbp.local>
2018-11-20 09:30:24 +0200

    Small changes to the Header component

--- src/commons/header/Header.tsx
+++ src/commons/header/Header.tsx
@@ -1,18 +1,56 @@
 import {Component} from "react";
+import * as React from "react";
+import './Header.css';
+import logo from './logo.png';
 
 class Header extends Component<any,any>{
 
+    constructor(props: any){
+      super(props);
+      this.state = {
+          displayCourses: 'none',
+
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        console.log("It's working!")
+    }
+
+
     render() {
         return (
-            <div className="container">
-            <div className="container dashboard-course-item">
-            <div className="dashboard-course-item-text">{this.name}</div>
-        <div className="dashboard-course-item-text">{this.department}</div>
-        <div className="div-config-button-wrapper">
-        <button className="course-config-button-wrapper">{this.button()}</button>
-        </div>
-        </div>
-        </div>
+            <div>
+                <header className="p-app-header">
+                    <div className="p-img-div">
+                        <img src={logo} className="p-app-logo"/>
+                    </div>
+
+                    <div className="p-menu-buttons-div">
+                        <div className="p-header-button" onClick={this.handleClick}>
+                            <div>LOGIN</div>
+                            <div id="login" className="hero" style={{display:'inline'}}></div>
+                        </div>
+                        <div className="p-header-button">
+                            <div>COURSES</div>
+                            <div id="courses" className="hero"></div>
+                        </div>
+                        <div className="p-header-button">
+                            <div>STUDENTS</div>
+                            <div id="student" className="hero"></div>
+                        </div>
+                        <div className="p-header-button">
+                            <div>CATALOG</div>
+                            <div id="catalog" className="hero"></div>
+                        </div>
+                        <div className="p-header-button">
+                            <div>MY PROFILE</div>
+                            <div id="myprofile" className="hero"></div>
+                        </div>
+                    </div>
+                </header>
+            </div>
     );
     }
 }
