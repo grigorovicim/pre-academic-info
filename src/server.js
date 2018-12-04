0e9d61c5f261c7091d70d94ddd0ca7a4c1870240
analalalah <ana96.stanciu@gmail.com>
2018-12-04 11:19:23 +0200

    users-api-calls with Sequelize

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ const jsSHA = require("jssha");
 var StudentRoutes  = require('./routes/student-routes');
 var ProfessorRoutes  = require('./routes/professor-routes');
 var CourseRoutes  = require('./routes/course-routes');
+var UserRoutes = require('./routes/user-routes');
 
 const app = express();
 
@@ -33,6 +34,7 @@ const dev = app.get('env') !== 'production';
 app.use('/student', StudentRoutes);
 app.use('/professor', ProfessorRoutes);
 app.use('/course', CourseRoutes);
+app.use('/user', UserRoutes);
 
 app.get('/check-server', (req, res) => {
   res.send({ express: 'Hello From Express BACKEND!' });
