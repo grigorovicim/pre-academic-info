f8e5a9924de47dc6ecd6e5c0c9ab0a8a0d7ec3ed
monicagrig <monica.grigorovici@telenav.com>
2018-12-06 21:09:14 +0200

    - catalog component with all courses of current user logged in - filter for assigned courses of professor

--- src/reducers/courseReducer.ts
+++ src/reducers/courseReducer.ts
@@ -1,28 +1,49 @@
 
-const courseReducer = (state: {professors: {};} = {
-    professors: {
+import { FETCH_CATALOG, FILTER_CATALOG_STUDY, FILTER_CATALOG_YEAR, FILTER_CATALOG_SEMESTER } from '../actions/types';
+
+const initialState = {
+    items: [
+        {id:1, name: "LFTC", department: "info", isConfigured: false},
+        {id:2, name: "PPD", department: "info", isConfigured: false},
+        {id:3, name: "PLF", department: "info", isConfigured: true},
+        {id:4, name: "Microcontrollers", department: "info", isConfigured: false},
+        {id:5, name: "ASC", department: "info", isConfigured: true},
+        {id:6, name: "MAP", department: "info", isConfigured: true},
+    ],
+    study: "LICENTA",
+    year: "I",
+    semester: "I",
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_CATALOG:
+            return {
+                ...state,
+                items: action.payload.items
+            }
     
-    }
-}, action: any) => {
+        case FILTER_CATALOG_STUDY:
+            return { 
+                ...state,
+                study: action.payload.study
+            }
 
-    const {
-      type,
-      payload,
-    } = action;
-  
-    //dummy code; all the actions affecting users will need to be treated here
-    if (type === '' && payload === '')
-    {
-        return {
+        case FILTER_CATALOG_YEAR: {
+            return {
+                ...state,
+                year: action.payload.year
+            }
+        }
 
-        };
+        case FILTER_CATALOG_SEMESTER: {
+            return {
+                ...state,
+                semester: action.payload.semester
+            }
+        }
+
+        default:
+            return state;
     }
-      
-      return {
-          
-      };
-  
-  
-  };
-  
-  export default courseReducer;
+};
