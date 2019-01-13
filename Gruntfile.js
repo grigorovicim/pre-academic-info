6c68e21f0610fa078736cfe87914096c471b4e5d
Emilia Hangan <emilia.hangan98@gmail.com>
2019-01-13 15:34:34 +0200

    Added mock data

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,24 @@ module.exports = function(grunt) {
     grunt.initConfig({
         fixtures: {
             import_test_data: {
-                src: ['./src/fixtures/*.json'],
+                src: [
+                    './src/fixtures/groups.json',
+                    './src/fixtures/year_of_studies.json',
+                    './src/fixtures/semesters.json',
+                    './src/fixtures/abbreviations.json',
+                    './src/fixtures/sections.json',
+                    './src/fixtures/roles.json',
+                    //'./src/fixtures/users.json',
+                    //'./src/fixtures/profiles.json',
+                    //'./src/fixtures/titles.json',
+                    //'./src/fixtures/professors.json',
+                    //'./src/fixtures/students.json',
+                    //'./src/fixtures/academic_programmes.json',
+                    //'./src/fixtures/courses.json',
+                    //'./src/fixtures/student_courses.json',
+                    './src/fixtures/professor_courses.json',
+
+                ],
                 models: function () {
                     return require('./src/models')
                 },
