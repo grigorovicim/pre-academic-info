6f4b8ba1b990ac5800769aa0b86eada815d63e99
nikkied <nikkiedolot@gmail.com>
2019-01-19 10:54:59 +0200

    Created course-detail-routes file.

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,19 @@ class App extends Component<any, any> {
     };
 
     public render() {
-        return (
+        const detail = {
+            name: "Design Patterns",
+            professor: "Molnar Arthur",
+            section: {name: 'English', nrGroups: 6},//nrGroups not in db
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            rules: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            courses: {number: 14, hours: 2, tests: 10, percentage: 60, writtenExam: 50, practicalExam: 20},
+            labs: {number: 7, hours: 2, tests: 12, percentage: 30, professors: [{id: 1, name: "Molnar Arthur"}, {id: 2, name: "Cretu Maria"}]},
+            seminars: {number: 14, hours: 2, tests: 2, percentage: 10, professors: {id: 1, name: "Molnar Arthur"}},
+            groups: [932, 933, 934, 935],
+            students: [{id: 1, name: "Antonesei Andrada"},{id:2, name: "Amariei Iuliana"},{id:3, name: "Blanariu Mihai"}]
+          }
+          return (
             <div className="p-app">
                 <BrowserRouter>
                     <div>
