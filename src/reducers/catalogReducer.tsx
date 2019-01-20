b980bf4de2a776a33661f4c5f02b16321b56862b
Emilia Hangan <emilia.hangan98@gmail.com>
2019-01-20 08:28:29 +0200

    WIP

--- src/reducers/catalogReducer.tsx
+++ src/reducers/catalogReducer.tsx
@@ -3,7 +3,7 @@ import { FETCH_ACTIVITIES } from '../actions/types';
 const initialState = {
     items: [] as any,
     group: "17",
-    classType: "Courses",
+    classType: "Course",
     week: "1",
     studentSubstring: "",
 };
@@ -15,6 +15,26 @@ export default (state = initialState, action: any) => {
                 ...state,
                 items: Array.from(action.payload.items),
             };
+        case 'SAVE_GROUP':
+            return {
+                ...state,
+                group: action.payload.group
+            }
+        case 'SAVE_CLASS':
+            return {
+                ...state,
+                classType: action.payload.classType
+            }
+        case 'SAVE_WEEK':
+            return {
+                ...state,
+                week: action.payload.week
+            }
+        case 'SAVE_STUDENT_SUBSTRING':
+            return {
+                ...state,
+                studentSubstring: action.payload.str
+            }
 
         default:
             return state;
